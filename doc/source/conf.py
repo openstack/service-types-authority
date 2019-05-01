7d8a34237f38b9ac33d610cf68421a44663984c8
Andreas Jaeger <aj@suse.com>
2019-05-01 19:58:24 +0200

    Switch to openstackdocstheme
    Remove oslosphinx, it is deprecated and does not work with Sphinx 2.0.
    Instead use openstackdocstheme.
    
    Use newer sphinx-build in tox docs environment following PTI.
    
    Change-Id: Ib19f12e5603b7b5c787cafd77551eb3cf7fbc4cd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['oslosphinx']
+extensions = ['openstackdocstheme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -36,6 +36,11 @@ source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
+
+# openstackdocstheme options
+repository_name = 'openstack/service-types-authority'
+bug_project = ''
+bug_tag = ''
 
 # General information about the project.
 project = u'Service Types Authority'
@@ -84,7 +89,7 @@ man_pages = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
