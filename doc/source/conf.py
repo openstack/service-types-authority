c99dfcf7a7da49aafd2120b866b2e360cc625daf
Andreas Jaeger <aj@suse.com>
2020-06-10 16:08:26 +0200

    Switch to newer openstackdocstheme version
    Switch to openstackdocstheme 2.2.1 version. Using
    this version will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Disable openstackdocs_auto_version to not auto-version the documents.
    
    Disable openstackdocs_auto_name to use 'project' variable as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I7f07fc60f45deb2a415af1216ab5b37398cb2929

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,9 +35,14 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # openstackdocstheme options
-repository_name = 'openstack/service-types-authority'
-bug_project = ''
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/service-types-authority'
+openstackdocs_auto_name = False
+openstackdocs_auto_version = False
+openstackdocs_bug_project = ''
+openstackdocs_bug_tag = ''
+
+version = ''
+release = ''
 
 # General information about the project.
 project = u'Service Types Authority'
@@ -74,7 +79,7 @@ add_module_names = False
 show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 modindex_common_prefix = []
